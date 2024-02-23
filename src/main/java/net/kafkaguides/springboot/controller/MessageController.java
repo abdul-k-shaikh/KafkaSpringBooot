4ed6ac4456c4e39004036c16e5ebff57720c2b68
theabdul97@gmail.com <theabdul97@gmail.com>
2024-02-24 00:00:48 +0530

    dummy

--- src/main/java/net/kafkaguides/springboot/controller/MessageController.java
+++ src/main/java/net/kafkaguides/springboot/controller/MessageController.java
@@ -16,6 +16,7 @@ public class MessageController {
 	
 	public MessageController(KafkaProducer kafkaProducer) {
 		this.kafkaProducer = kafkaProducer;
+		testetinf ajadjadhfk 
 	}
 	
 	//http:localhost:8080/api/v1/kafka/publish?message=hello world
